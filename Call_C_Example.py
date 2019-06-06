1362109423d2cd65a74d8529db88847b1e8b5e84
Anj <subedianjan@yahoo.com.au>
2019-06-06 18:22:20 +1000

    Test executing GCC
    execute c program from C ompiler

--- Call_C_Example.py
+++ Call_C_Example.py
@@ -5,7 +5,7 @@ libtest2.so is shared library file produced from GCC with the test2.c file.
 Check the Calling C functions*.html file for the detailed instructions.
 
 '''
-
+import sys, string, os
 from ctypes import *
 from cffi import FFI
 import random
@@ -15,5 +15,14 @@ from sys import *
 libCalc = CDLL("C:\\Projects\\GCC\\Maths\libtest2.so")
 #call C function to check connection
 print ( libCalc.add(1,2) )
+
+#Compiling and Executing a C program
+os.chdir('C:\\Projects\\GCC\\Maths')
+os.system ("gcc.exe -o test2.exe test2.c")
+os.system ("test2.exe")
+
+
+#os.system("C:\\Projects\\GCC\gcc\bin\gcc.exe" )
+#-o test2.exe test2.c"')
 
 
